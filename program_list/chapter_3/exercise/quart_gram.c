a440b9a77639d07917dcb90aca8ad4c0529c2535
jack <jackyhoikiu@gmail.com>
2020-01-24 23:05:10 +0800

    fixed a problem

--- program_list/chapter_3/exercise/quart_gram.c
+++ program_list/chapter_3/exercise/quart_gram.c
@@ -4,13 +4,13 @@ int main(void)
 	float quart;
 	float molecule;
 	//float mass_mol = 3.0e-23;
-	molecule = quart * 950 / 3.0e-23;
 
 	printf("how many quarts of water do you have?\n");
 	printf("I got ____ quarts of water.\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
 	scanf("%f", &quart);
+	molecule = quart * 950 / 3.0e-23;
 	printf("Ok, do you know how many water molecules in it?\n");
-	printf("Let me tell you, there are about %e water molecules in it!", molecule);
+	printf("Let me tell you, there are about %e water molecules in it!\n", molecule);
 
 	return 0;
 }
